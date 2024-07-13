6bb390034142c599efe3e0125954c057a5d213b2
franz-Kosy <franzmesh5@gmail.com>
2024-07-13 15:36:43 +0100

    Fourth Commit

--- carousel.js
+++ carousel.js
@@ -24,3 +24,4 @@ document.addEventListener('DOMContentLoaded', () => {
 
     setInterval(showNextImage, 3000);
 });
+
--- script.js
+++ script.js
@@ -1,29 +1,13 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Function to fetch and populate countries
-    async function populateCountries() {
-        try {
-            console.log('Fetching countries...');
-            const response = await fetch('https://api.first.org/data/v1/countries');
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
-            const data = await response.json();
-            const countries = data.data;
-            console.log('Countries fetched:', countries);
+let hamburger = document.querySelector(".hamburger");
+let mobileNav = document.querySelector(".mobileNav");
+let cancelBtn = document.querySelector(".cancelBtn");
+console.log(mobileNav);
+console.log(hamburger);
 
-            const countrySelect = document.getElementById('country');
-            Object.keys(countries).forEach(key => {
-                const country = countries[key];
-                const option = document.createElement('option');
-                option.value = key; // ISO 3166-1 alpha-2 country code
-                option.textContent = country.country; // Common name of the country
-                countrySelect.appendChild(option);
-            });
-        } catch (error) {
-            console.error('Error fetching countries:', error);
-        }
-    }
-
-    // Call the function to populate the dropdown
-    populateCountries();
+hamburger.addEventListener("click", () =>{
+    mobileNav.classList.add("show");
 });
+
+cancelBtn.addEventListener("click", () =>{
+    mobileNav.classList.remove("show");
+})
