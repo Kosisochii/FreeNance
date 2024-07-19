d04887558528418af31869a07cfda4a749448e87
franz-Kosy <franzmesh5@gmail.com>
2024-07-20 00:37:16 +0100

    fifth commit

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 let hamburger = document.querySelector(".hamburger");
 let mobileNav = document.querySelector(".mobileNav");
 let cancelBtn = document.querySelector(".cancelBtn");
-console.log(mobileNav);
-console.log(hamburger);
 
 hamburger.addEventListener("click", () =>{
     mobileNav.classList.add("show");
